5c33f82f2a26522b6a77758bc52c46b309d8bb8b
Samuel Lidén Borell <samuel@slbdata.se>
2010-09-24 18:11:06 +0200

    Add a test of moving boxes away from the goal

--- sokoban/test/BoardTest.java
+++ sokoban/test/BoardTest.java
@@ -11,7 +11,7 @@ import sokoban.Board;
 public class BoardTest
 {
 
-    Board b1;
+    Board b1, b2;
 
     @Before
     public void setUp() throws Exception
@@ -46,6 +46,39 @@ public class BoardTest
         
         b1.refresh();
         assertEquals(1, b1.getRemainingBoxes());
+        
+        
+        b2 = new Board(4, 5, 1, 1);
+        b2.cells[0][0] = Board.WALL;
+        b2.cells[0][1] = Board.WALL;
+        b2.cells[0][2] = Board.WALL;
+        b2.cells[0][3] = Board.WALL;
+
+        b2.cells[1][0] = Board.WALL;
+        b2.cells[1][1] = 0;
+        b2.cells[1][2] = 0;
+        b2.cells[1][3] = Board.WALL;
+
+        b2.cells[2][0] = Board.WALL;
+        b2.cells[2][1] = 0;
+        b2.cells[2][2] = Board.BOX | Board.GOAL;
+        b2.cells[2][3] = Board.WALL;
+
+        b2.cells[3][0] = Board.WALL;
+        b2.cells[3][1] = 0;
+        b2.cells[3][2] = 0;
+        b2.cells[3][3] = Board.WALL;
+
+        b2.cells[4][0] = Board.WALL;
+        b2.cells[4][1] = Board.WALL;
+        b2.cells[4][2] = Board.WALL;
+        b2.cells[4][3] = Board.WALL;
+        b2.playerCol = 1;
+        b2.playerRow = 1;
+        
+        b2.refresh();
+        assertEquals(0, b2.getRemainingBoxes());
+
     }
 
     @After
@@ -78,7 +111,7 @@ public class BoardTest
     }
     
     @Test
-    public void move()
+    public void moveToGoal()
     {
         b1.move(Board.Direction.RIGHT);
         assertEquals(1, b1.playerRow);
@@ -99,6 +132,30 @@ public class BoardTest
         assertEquals(3, b1.playerRow);
         assertEquals(1, b1.playerCol);
         assertEquals(0, b1.getRemainingBoxes());
+    }
+    
+    @Test
+    public void moveFromGoal()
+    {
+        b2.move(Board.Direction.DOWN);
+        assertEquals(2, b2.playerRow);
+        assertEquals(1, b2.playerCol);
+        assertEquals(0, b2.getRemainingBoxes());
+        
+        b2.move(Board.Direction.DOWN);
+        assertEquals(3, b2.playerRow);
+        assertEquals(1, b2.playerCol);
+        assertEquals(0, b2.getRemainingBoxes());
+        
+        b2.move(Board.Direction.RIGHT);
+        assertEquals(3, b2.playerRow);
+        assertEquals(2, b2.playerCol);
+        assertEquals(0, b2.getRemainingBoxes());
+        
+        b2.move(Board.Direction.UP);
+        assertEquals(2, b2.playerRow);
+        assertEquals(2, b2.playerCol);
+        assertEquals(1, b2.getRemainingBoxes());
     }
 
 }
