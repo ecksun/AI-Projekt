35afbe038a983ec311d69f75df6d957d5f216829
Joel Pettersson <petterssonjoel@gmail.com>
2010-09-24 14:49:28 +0200

    Add missing file (IDS.java).

---
+++ sokoban/IDS.java
@@ -0,0 +1,24 @@
+package sokoban;
+
+import sokoban.solvers.Solver;
+
+public class IDS implements Solver
+{
+
+    private Board startBoard;
+    
+    public IDS(Board startBoard)
+    {
+        this.startBoard = startBoard;
+    }
+
+    @Override
+    public String solve()
+    {
+
+        System.out.println(startBoard);
+        
+        return null;
+    }
+
+}
