b0981bb9f91eee9344394406ab9c04ae0523b890
Samuel Lidén Borell <samuel@slbdata.se>
2010-09-24 11:04:11 +0200

    Width and height fields on the board

--- Board.java
+++ Board.java
@@ -3,14 +3,17 @@
  */
 public class Board
 {
-    // Input values 
+    // Input values
     final byte WALL = 0x01;
     final byte BOX =  0x02;
     final byte GOAL = 0x04;
     // Generated values
     final byte NO_BOX = 0x08; 
-    // Bitmasks 
+    // Bitmasks
     final byte OCCUPIED = 0x03;
+    
+    final int width;
+    final int height;
     
     byte cells[][];
     int  playerX;
@@ -20,6 +23,9 @@ public class Board
     Board(int x, int y, int playerX, int playerY)
     {
         cells = new byte[x][y];
+        this.height = cells.length;
+        this.width = cells[0].length;
+        
         this.playerX = playerX;
         this.playerY = playerY;
     }
