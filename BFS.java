617d2e85eca4d88c0de854b29913c05d8095f633
Joel Bohman <jbohman@csc.kth.se>
2010-09-24 11:47:06 +0200

    Added BFS class

---
+++ BFS.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ *
+ */
+public class BFS implements Solver
+{
+
+    /**
+     * @see Solver#solve()
+     */
+    @Override
+    public String solve()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
