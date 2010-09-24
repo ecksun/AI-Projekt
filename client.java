eb26b46b73b6610f077da70fac5592b1e936d357
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 10:12:58 +0200

    Fixed some formatting stuff

--- client.java
+++ client.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class client
 {
