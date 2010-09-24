5091a41131a8f3bf8adbaa6c33ec10ce883261d1
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 10:42:03 +0200

    Began on  a mail class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        if (args.length < 1) {
+            System.err.println("You need to supply board number as argument");
+            return;
+        }
+    }
+}
