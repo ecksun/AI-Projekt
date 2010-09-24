1250040f43fe1e5a93e3860099a95e6c2a75f617
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 11:30:26 +0200

    added toString to Board

--- Board.java
+++ Board.java
@@ -4,21 +4,21 @@
 public class Board
 {
     // Input values
-    final byte WALL = 0x01;
-    final byte BOX =  0x02;
-    final byte GOAL = 0x04;
+    final static byte WALL     = 0x01;
+    final static byte BOX      = 0x02;
+    final static byte GOAL     = 0x04;
     // Generated values
-    final byte NO_BOX = 0x08; 
+    final byte        NO_BOX   = 0x08;
     // Bitmasks
-    final byte OCCUPIED = 0x03;
-    
-    final int width;
-    final int height;
-    
-    byte cells[][];
-    int  playerX;
-    int  playerY;
-    int  remainingBoxes;
+    final byte        OCCUPIED = 0x03;
+
+    final int         width;
+    final int         height;
+
+    byte              cells[][];
+    int               playerX;
+    int               playerY;
+    int               remainingBoxes;
 
     /**
      * Initialize a new board
@@ -33,29 +33,50 @@ public class Board
         cells = new byte[x][y];
         this.height = cells.length;
         this.width = cells[0].length;
-        
+
         this.playerX = playerX;
         this.playerY = playerY;
     }
-    
+
     /**
      * Updates some variables after a board has been loaded.
      */
-    public void refresh() {
+    public void refresh()
+    {
         countBoxes();
     }
-    
+
     /**
-     * Counts the boxes that are not in a goal, and updates remainingBoxes. 
+     * Counts the boxes that are not in a goal, and updates remainingBoxes.
      */
     private void countBoxes()
     {
         int remaining = 0;
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if ((cells[y][x] & (BOX | GOAL)) == BOX) remaining++;
+                if ((cells[y][x] & (BOX | GOAL)) == BOX) {
+                    remaining++;
+                }
             }
         }
         remainingBoxes = remaining;
+    }
+
+    /**
+     * Print the current board
+     * 
+     * @return The string representing the board
+     */
+    @Override
+    public String toString()
+    {
+        String tmp = "";
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                tmp += cells[i][j];
+            }
+            tmp += "\n";
+        }
+        return tmp;
     }
 }
