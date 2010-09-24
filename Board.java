cec24f7beef2242e3513d39b943111eae4c630bb
Samuel Lidén Borell <samuel@slbdata.se>
2010-09-24 10:59:05 +0200

    Add board constants for walls, boxes, etc

--- Board.java
+++ Board.java
@@ -3,6 +3,15 @@
  */
 public class Board
 {
+    // Input values 
+    final byte WALL = 0x01;
+    final byte BOX =  0x02;
+    final byte GOAL = 0x04;
+    // Generated values
+    final byte NO_BOX = 0x08; 
+    // Bitmasks 
+    final byte OCCUPIED = 0x03;
+    
     byte cells[][];
     int  playerX;
     int  playerY;
