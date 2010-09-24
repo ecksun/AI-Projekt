7e54e9f4513236fd09efdbd30f198776a33744a2
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 11:15:12 +0200

    Added comments

--- Board.java
+++ Board.java
@@ -8,6 +8,14 @@ public class Board
     int  playerY;
     int  remainingBoxes;
 
+    /**
+     * Initialize a new board
+     * 
+     * @param x The width of the board
+     * @param y The height of the board
+     * @param playerX The X position of the player
+     * @param playerY The Y position of the player
+     */
     Board(int x, int y, int playerX, int playerY)
     {
         cells = new byte[x][y];
