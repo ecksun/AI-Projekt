83fd48c90861f00b4eb4e3c6de5daad0a1a2b467
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 09:57:33 +0200

    Renamed the code style stuff and formated the client

--- client.java
+++ client.java
@@ -4,7 +4,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class client {
+public class client
+{
 
     public static void main(String[] args)
     {
@@ -16,31 +17,34 @@ public class client {
             System.err.println("You need to supply board number as argument");
             return;
         }
-        
+
         try {
-            Socket socket = new Socket("cvap103.nada.kth.se",5555);
+            Socket socket = new Socket("cvap103.nada.kth.se", 5555);
             InputStream inRaw = socket.getInputStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(inRaw));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             out.println(args[0]);
-            
+
             inRaw.read(boardbytes);
             board = new String(boardbytes);
             System.out.println(board);
-            
-            //String solution = new String("0 3 3 1 0 0 2 1 2 2 0 2 2 1 3 3 3 3 2 1 1 3 0 3 0 1 2 2 0 3");
-			String solution = new String("U R R D U U L D L L U L L D R R R R L D D R U R U D L L U R");
-            
+
+            // String solution = new
+            // String("0 3 3 1 0 0 2 1 2 2 0 2 2 1 3 3 3 3 2 1 1 3 0 3 0 1 2 2 0 3");
+            String solution = new String(
+                    "U R R D U U L D L L U L L D R R R R L D D R U R U D L L U R");
+
             out.println(solution);
-            
+
             result = in.readLine();
             System.out.println(result);
-            
+
             out.close();
             in.close();
             socket.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
