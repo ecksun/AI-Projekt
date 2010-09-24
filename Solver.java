de1866c3ffea560338b937117382ee138e4c0a0b
Linus Wallgren <linus.wallgren@gmail.com>
2010-09-24 11:37:41 +0200

    Added a solver interface

---
+++ Solver.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * An interface for the different solver.
+ */
+public interface Solver
+{
+    /**
+     * Solve the board
+     * 
+     * @return A string corresponding to the movements of the player
+     */
+    String solve();
+}
