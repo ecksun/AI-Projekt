7a425f7c3cc0441f42598edbade9b18c26d009e8
Joel Pettersson <petterssonjoel@gmail.com>
2010-09-25 20:59:18 +0200

    Add method to get directions in random order.
    I tried using it on level 1, resulting in same maxDepth value and 10 ms
    slower in average (on 10 runs). However, this slowdown might be due to the
    shuffling operations.
    
    Even though not activated in this version, it might be a good idea
    thinking about how we select next direction to walk.

--- sokoban/solvers/IDS.java
+++ sokoban/solvers/IDS.java
@@ -2,46 +2,52 @@ package sokoban.solvers;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Random;
 
 import sokoban.Board;
+import sokoban.Board.Direction;
 
 public class IDS implements Solver
 {
 
     private final Board startBoard;
     private final int DEPTH_LIMIT = 1000;
-    
+    private Random rand = new Random();
+
     public IDS(Board startBoard)
     {
         this.startBoard = startBoard;
     }
-    
+
     /**
      * Recursive Depth-First algorithm
-     * @param maxDepth  The maximum depth.
+     * 
+     * @param maxDepth The maximum depth.
      * @return
      */
     private Deque<Board.Direction> dfs(Board board, int maxDepth)
     {
-        //System.out.println(board.toString());
-        
+        // System.out.println(board.toString());
+
         if (board.getRemainingBoxes() == 0) {
             // Found a solution
             return new LinkedList<Board.Direction>();
         }
-        
-        if (maxDepth == 0) return null;
-        
+
+        if (maxDepth == 0)
+            return null;
+
         for (Board.Direction dir : Board.Direction.values()) {
             // Check that the move is possible
-            if (!board.canMove(dir)) continue;
-            
+            if (!board.canMove(dir))
+                continue;
+
             // The move is possible
-            
+
             // Make the move on a copy of the board
-            Board successor = (Board)board.clone();
+            Board successor = (Board) board.clone();
             successor.move(dir);
-            
+
             // Recurse
             Deque<Board.Direction> sol = dfs(successor, maxDepth - 1);
             if (sol != null) {
@@ -49,19 +55,40 @@ public class IDS implements Solver
                 return sol;
             }
         }
-        
+
         return null;
     }
-    
+
+    /**
+     * Returns an array of Direction enums in random order.
+     * 
+     * @return An array of all Direction enums.
+     */
+    private Direction[] getDirectionsRandomly()
+    {
+        Direction[] directions = Board.Direction.values();
+
+        // Shuffle array
+        for (int i = 0; i < directions.length; i++) {
+            int randomPosition = rand.nextInt(directions.length);
+            Direction temp = directions[i];
+            directions[i] = directions[randomPosition];
+            directions[randomPosition] = temp;
+        }
+        
+        return directions;
+    }
+
     private static final char moveChars[] = { 'U', 'D', 'L', 'R' };
-    
+
     /**
      * 
-     * @param solution  An array from dfs()
+     * @param solution An array from dfs()
      * @return The solution as a string
      */
-    private String solutionToString(Deque<Board.Direction> solution) {
-        StringBuilder sb = new StringBuilder(2*solution.size());
+    private String solutionToString(Deque<Board.Direction> solution)
+    {
+        StringBuilder sb = new StringBuilder(2 * solution.size());
         for (Board.Direction move : solution) {
             sb.append(moveChars[move.ordinal()]);
             sb.append(' ');
@@ -77,11 +104,11 @@ public class IDS implements Solver
             Deque<Board.Direction> solution = dfs(startBoard, maxDepth);
             if (solution != null) {
                 System.out.println();
-                return solutionToString(solution); 
+                return solutionToString(solution);
             }
             System.out.print(maxDepth + ".");
         }
-        
+
         System.out.println("no solution!");
         return null;
     }
