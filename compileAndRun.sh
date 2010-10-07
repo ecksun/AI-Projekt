2021e0e315e8ef5bda5286c94e8cb80ec16c828a
Linus Wallgren <linuswa@kth.se>
2010-10-07 14:38:10 +0200

    Updated the compileandrun script
    It can now take arguments from the command line

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -n "$1" ]; then
+    BOARD=$1
+else
+    BOARD=3
+fi
+
 javac -sourcepath . sokoban/Main.java
-java -classpath . sokoban.Main 2
+java -classpath . sokoban.Main $BOARD
 
