f660f6fd6cddce97fe9cb021d4a747653a501fd5
Samuel Lidén Borell <samuel@slbdata.se>
2010-10-07 14:41:10 +0200

    Fix logic errors in Puller

--- sokoban/solvers/Puller.java
+++ sokoban/solvers/Puller.java
@@ -12,26 +12,35 @@ public class Puller implements Solver
     private final Board startBoard;
     private Board board;
     
-    private LinkedList<Box> boxes;
+    private LinkedList<Position> boxes;
     
-    public class Box {
+    public class Position {
         final int x, y;
-        public Box(int x, int y) {
+        public Position(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+    
+    public class PlayerPosDir extends Position {
+        final int bx, by; // relative box position
+        public PlayerPosDir(int x, int y, int bx, int by) {
+            super(x, y);
+            this.bx = bx;
+            this.by = by;
         }
     }
 
     public Puller(Board startBoard)
     {
         // Store all boxes
-        boxes = new LinkedList<Box>();
+        boxes = new LinkedList<Position>();
         this.startBoard = startBoard;
         
         for (int i = 0; i < board.height; ++i) {
             for (int j = 0; j < board.width; ++j) {
                 if (board.cells[i][j] == Board.BOX) {
-                    boxes.add(new Box(i, j));
+                    boxes.add(new Position(i, j));
                 }
             }
         }
@@ -51,8 +60,8 @@ public class Puller implements Solver
             reset();
             
             do {
-                Box box = chooseBox();
-                while (moveBox(box)) { }
+                PlayerPosDir playerPosDir = choosePosition();
+                while (moveBox(playerPosDir)) { }
                 
             } while (!deadlock());
         } while (!solved());
@@ -72,13 +81,13 @@ public class Puller implements Solver
         return false;
     }
 
-    private boolean moveBox(Box box)
+    private boolean moveBox(Position box)
     {
         // TODO Auto-generated method stub
         return false;
     }
 
-    private Box chooseBox()
+    private PlayerPosDir choosePosition()
     {
         // TODO Auto-generated method stub
         return null;
