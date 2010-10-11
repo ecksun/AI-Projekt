8597810bf99d5dfa5ca65c39d4f1d333751618d1
Linus Wallgren <linuswa@kth.se>
2010-10-11 13:48:26 +0200

    Added code skeleton for pusher

---
+++ sokoban/solvers/Pusher.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package sokoban.solvers;
+
+import sokoban.Board;
+
+/**
+ *
+ */
+public class Pusher implements Solver
+{
+
+    @Override
+    public int getIterationsCount()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String solve(Board board)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+   
+
+}
