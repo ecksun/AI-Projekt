c99d31632203605a1be7b996b1683b9f2a6f66bd
Joel Pettersson <petterssonjoel@gmail.com>
2010-10-12 09:14:00 +0200

    Organize imports in Main.java.

--- sokoban/Main.java
+++ sokoban/Main.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import sokoban.solvers.IDS;
-import sokoban.solvers.Puller;
 import sokoban.solvers.Solver;
 
 /**
