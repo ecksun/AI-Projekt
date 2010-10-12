6a7252d7d9301389e5966797fa8ea3d36c35b77f
Joel Pettersson <petterssonjoel@gmail.com>
2010-10-12 14:55:52 +0200

    Almost done with the basic bruteforce, not working
    It does find a sollution but does not print the path correctly

--- sokoban/solvers/Pusher.java
+++ sokoban/solvers/Pusher.java
@@ -4,11 +4,13 @@
 package sokoban.solvers;
 
 import java.util.Collection;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import sokoban.Board;
 import sokoban.Position;
+import sokoban.Board.Direction;
 
 /**
  * A solver that pushes boxes around.
@@ -25,7 +27,7 @@ public class Pusher implements Solver
         /**
          * The path to this searchnode
          */
-        public final String path;
+        public final Deque<Direction> path;
         /**
          * The board at this searchnode
          */
@@ -42,23 +44,32 @@ public class Pusher implements Solver
          * @param board The board at this searchnode
          * @param parent The parent searchnode
          */
-        public SearchNode(String path, Board board, SearchNode parent)
+        public SearchNode(Deque<Direction> path, Board board, SearchNode parent)
         {
+            System.out.println("Generating new node");
             this.path = path;
-            this.board = (Board) parent.board.clone();
+            this.board = (Board) board.clone();
             this.parent = parent;
         }
 
         /**
-         * Create a new SearchNode from a parent
+         * Create a new SearchNode from a parent and moves the player in the
+         * specified direction.
          * 
          * @param newPath The path from the parent to this node
-         * @param parent The parent
+         * @param dir The direction to move the player in.
+         * @param parent The parent state.
          */
-        public SearchNode(String newPath, SearchNode parent)
+        public SearchNode(Deque<Direction> newPath, Board.Direction dir,
+                Position playerStartPosition, SearchNode parent)
         {
             // TODO it is possible to generate the path from parent when needed.
-            this(parent.path + newPath, parent.board, parent);
+            this(new LinkedList<Direction>(), parent.board, parent);
+            board.playerCol = playerStartPosition.column;
+            board.playerRow = playerStartPosition.row;
+            path.addAll(newPath);
+            path.add(dir);
+            board.move(dir);
         }
     }
 
@@ -71,42 +82,75 @@ public class Pusher implements Solver
     public String solve(Board board)
     {
         Queue<SearchNode> queue = new LinkedList<SearchNode>();
-        queue.add(new SearchNode("", board, null));
+        queue.add(new SearchNode(new LinkedList<Direction>(), board, null));
         SearchNode node;
         do {
-            node = queue.poll();
-            queue.addAll(getAllBoxesMovement(node));
+            if ((node = queue.poll()) == null) {
+                return null;
+            }
+            queue.addAll(getAllSuccessorStates(node));
         }
         while (node.board.getRemainingBoxes() != 0);
-        return null;
+        System.out.println(Board.solutionToString(node.path));
+        return Board.solutionToString(node.path);
     }
 
     /**
-     * Return all possible movements of the boxes on the current board
+     * Finds all possible new states, expands and returns them for a specific
+     * search node.
      * 
      * @param board The board
      * @return A list of new SearchNodes
      */
-    private Collection<SearchNode> getAllBoxesMovement(SearchNode node)
+    private Collection<SearchNode> getAllSuccessorStates(SearchNode node)
     {
         Collection<SearchNode> tmp = new LinkedList<SearchNode>();
+        System.out.println("node:" + node);
+        System.out.println("node.board:\n" + node.board);
         for (Position box : node.board.getBoxes()) {
             // if (accessible(node.board, box)
-            
+
+            Deque<Direction> path;
+            Position player;
             // This might be a problem with array index out of bounds if the box
             // is right next to the end of the board
             if (!Board.is(node.board.cells[box.row + 1][box.column],
-                    Board.REJECT_BOX))
-                tmp.add(new SearchNode("U", node));
+                    Board.REJECT_BOX)) {
+                System.out.println("Board.is down");
+                player = new Position(box.row - 1, box.column);
+                if ((path = node.board.findPath(player)) != null) {
+                    System.out.println("Baord.is down foudn path");
+                    tmp.add(new SearchNode(path, Direction.DOWN, player, node));
+                }
+            }
             if (!Board.is(node.board.cells[box.row - 1][box.column],
-                    Board.REJECT_BOX))
-                tmp.add(new SearchNode("D", node));
+                    Board.REJECT_BOX)) {
+                System.out.println("board is up");
+                player = new Position(box.row + 1, box.column);
+                if ((path = node.board.findPath(player)) != null) {
+                    System.out.println("board is up found path");
+                    tmp.add(new SearchNode(path, Direction.UP, player, node));
+                }
+            }
             if (!Board.is(node.board.cells[box.row][box.column + 1],
-                    Board.REJECT_BOX))
-                tmp.add(new SearchNode("R", node));
+                    Board.REJECT_BOX)) {
+                player = new Position(box.row, box.column - 1);
+                if ((path = node.board.findPath(player)) != null) {
+                    tmp
+                            .add(new SearchNode(path, Direction.RIGHT, player,
+                                    node));
+
+                }
+            }
             if (!Board.is(node.board.cells[box.row][box.column - 1],
-                    Board.REJECT_BOX))
-                tmp.add(new SearchNode("L", node));
+                    Board.REJECT_BOX)) {
+                player = new Position(box.row, box.column + 1);
+                if ((path = node.board.findPath(player)) != null) {
+                    tmp.add(new SearchNode(path, Direction.LEFT, player, node));
+
+                }
+            }
+
         }
         return tmp;
     }
