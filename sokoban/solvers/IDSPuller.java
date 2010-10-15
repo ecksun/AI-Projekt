23435a9143bf0f712bb2b22ba090df95b2ea4d56
Joel Pettersson <petterssonjoel@gmail.com>
2010-10-16 00:00:56 +0200

    Extract common parts of IDS puller and pusher.

--- sokoban/solvers/IDSPuller.java
+++ sokoban/solvers/IDSPuller.java
@@ -1,11 +1,8 @@
 package sokoban.solvers;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 
 import sokoban.Board;
 import sokoban.Position;
@@ -17,37 +14,17 @@ import sokoban.Board.Direction;
  * A solver that pulls boxes around (it finds a path from the goal to the
  * start) with iterative deepening and hashing to avoid duplicate states.
  */
-public class IDSPuller implements Solver
+public class IDSPuller extends IDSCommon implements Solver
 {
-    private final int DEPTH_LIMIT = 1000;
-    /**
-     * The number of generated nodes
-     */
-    public static int generatedNodes = 0;
+
     private static int depth, maxDepth;
-    private static Board board;
     
     // Extra information for the puller
     private int boxesNotInStart, initialBoxesNotInStart;
     private boolean[][] boxStart;
     private Position playerStart;
 
-    public int getIterationsCount()
-    {
-        return generatedNodes;
-    }
-
-    /**
-     * Set of visited boards, including the player position
-     */
-    private HashSet<Long> visitedBoards;
-
-    /**
-     * Boards that just lead to deadlocks or already visited boards. It
-     * doesn't make sense to visit these in later iterations.
-     */
-    private HashSet<Long> failedBoards;
-
+ 
     /**
      * Recursive Depth-First algorithm
      * 
@@ -245,54 +222,5 @@ public class IDSPuller implements Solver
         board.forceReachabilityUpdate();
     }
 
-    private static int lowerBound(final Board board)
-    {
-        final ArrayList<Position> boxes = new ArrayList<Position>();
-        final Queue<Position> goals = new LinkedList<Position>();
-        for (int row = 0; row < board.height; row++) {
-            for (int col = 0; col < board.width; col++) {
-                if (Board.is(board.cells[row][col], Board.BOX)) {
-                    boxes.add(board.positions[row][col]);
-                }
-                if (Board.is(board.cells[row][col], Board.GOAL)) {
-                    goals.add(board.positions[row][col]);
-                }
-            }
-        }
-        int result = 0;
-        while (!goals.isEmpty()) {
-            final Position goal = goals.poll();
-            Position minBox = null;
-            int min = Integer.MAX_VALUE;
-            for (final Position box : boxes) {
-                final int tmp = distance(goal, box);
-                if (tmp < min) {
-                    min = tmp;
-                    minBox = box;
-                }
-            }
 
-            boxes.remove(minBox);
-            result += min;
-        }
-        return result;
-    }
-
-    /**
-     * Approximate the distance between two positions
-     * 
-     * The distance will be the absolute minimum and are guaranteed to be equal
-     * to or greater then the real distance.
-     * 
-     * TODO It might be smarter to implement a search that takes the actual
-     * board into account.
-     * 
-     * @param a One of the positions
-     * @param b The other position
-     * @return The approximate distance between the two.
-     */
-    private static int distance(final Position a, final Position b)
-    {
-        return Math.abs(a.column - b.column) + Math.abs(a.row - b.row);
-    }
 }
